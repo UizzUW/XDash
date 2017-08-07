3d2019954f76de386cb179b7cd9b826e6f06ea08
UizzUW <uizz.uw2@gmail.com>
2017-08-07 22:51:34 +0300

    add test project

---
+++ XDash.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace XDash.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(true, !false);
+        }
+    }
+}
